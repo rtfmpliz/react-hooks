66b6875f6261f0c83c0c152c25a9c886eec2c79a
wahyu <wahyu@istw.co.id>
2020-09-14 07:16:22 +0700

    begini bisa

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View USer</h2>
-          <UserTable users={users}/>
+          <UserTable users={user}/>
         </div>
       </div>
     </div>
