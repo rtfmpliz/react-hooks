a6a32d5ef6c5cae34ffde2cb724e0b693eb92040
wahyu <wahyu@istw.co.id>
2020-09-14 07:41:54 +0700

    bisa tapi tidak keluar ketika ketik di form input

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,12 +1,38 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+
+
 
 const AddUserForm = (props) => {
+    const initialFormState = { id: null, name: '', username: '' }
+    const [user, setUser] = useState(initialFormState)
+    
+    const handleInputChange = (event) => {
+        const { name, value } = event.target
+        setUser({ ...user, [name]: value })
+    }
+
     return (
-        <form>
+        <form
+        onSubmit={(event) => {
+            event.preventDefault()
+            if (!user.name || !user.username) return
+
+            props.addUser(user)
+            setUser(initialFormState)
+        }}
+        >
             <label>NAme</label>
-            <input type="text" name="name" value=""/>
+            <input
+                type="text"
+                name="name"
+                value=""
+                onChange={handleInputChange} />
             <label>UserNAme</label>
-            <input type="text" name="username" value=""/>
+            <input
+                type="text"
+                name="username"
+                value=""
+                onChange={handleInputChange} />
             <button>Add new user</button>
         </form>
     )
